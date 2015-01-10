580576d6d90eeeceef88785b6df82dc95d0c496a
kperreau <kperreau@student.42.fr>
2015-01-11 00:38:27 +0100

    auto update

--- semestre-1/printf/src/ft_type_o.c
+++ semestre-1/printf/src/ft_type_o.c
@@ -17,12 +17,14 @@ static int		ft_write_o(t_options *opt, unsigned long long n)
 	int		len;
 	int		temp;
 
-	temp = ft_nbrlen2(n);
+	temp = ((opt->flags & 8) ? 1 : 0) + ft_nbrlen2(n);
 	len = ((opt->precise > temp) ? opt->precise : temp);
 	if (opt->len > len && !(opt->flags & 1))
 		ft_putspace(opt, len, 0);
 	if (opt->precise > temp)
 		ft_putzero(opt, temp);
+	if (opt->flags & 8)
+		write(1, "0", 2);
 	ft_putnbrul(n);
 	if (opt->len > len && opt->flags & 1)
 		ft_putspace(opt, len, 0);
@@ -47,5 +49,5 @@ int			ft_o2(t_options *opt, va_list *ap, int *ret)
 {
 	opt->modif = 1;
 	opt->type = 6;
-	return (ft_u(opt, ap, ret));
+	return (ft_o(opt, ap, ret));
 }
