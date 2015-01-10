46c9a3c1d5e9b701019ae62ef7365a092f1d8a6f
kperreau <kperreau@student.42.fr>
2015-01-11 00:42:04 +0100

    auto update

--- semestre-1/printf/src/ft_types_x.c
+++ semestre-1/printf/src/ft_types_x.c
@@ -12,7 +12,7 @@
 
 #include "printf.h"
 
-static int		ft_write_x(t_options *opt, char *hex)
+/*static int		ft_write_x(t_options *opt, char *hex)
 {
 	int		len;
 	int		temp;
@@ -28,6 +28,30 @@ static int		ft_write_x(t_options *opt, char *hex)
 		ft_putspace(opt, len, 0);
 	len = (opt->len > len) ? opt->len : len;
 	return (len);
+}*/
+
+static int		ft_write_x(t_options *opt, char *hex)
+{
+	int		len;
+	int		temp;
+
+	if (opt->len && opt->flags & 8)
+		opt->len -= 2;
+	if (opt->precise == -1 && opt->zero && opt->flags & 8)
+		opt->precise = opt->len;
+	temp = ft_strlen(hex);
+	len = ((opt->precise > temp) ? opt->precise : temp);
+	if (opt->len > len && !(opt->flags & 1))
+		ft_putspace(opt, len, 0);
+	if (opt->flags & 8)
+		write(1, "0x", 2);
+	if (opt->precise > temp)
+		ft_putzero(opt, temp);
+	write(1, hex, temp);
+	if (opt->len > len && opt->flags & 1)
+		ft_putspace(opt, len, 0);
+	len = (opt->len > len) ? opt->len : len;
+	return (len + 2);
 }
 
 int			ft_x(t_options *opt, va_list *ap, int *ret)
