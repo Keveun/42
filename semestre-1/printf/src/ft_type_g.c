38df9a26156ccbf3f3ac0e417de1f08995504867
Kevin Perreau <Keveun@gmail.com>
2015-01-16 00:36:17 +0100

    auto update

--- semestre-1/printf/src/ft_type_g.c
+++ semestre-1/printf/src/ft_type_g.c
@@ -6,7 +6,7 @@
 /*   By: kperreau <kperreau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/01/15 20:33:33 by kperreau          #+#    #+#             */
-/*   Updated: 2015/01/16 00:32:19 by kperreau         ###   ########.fr       */
+/*   Updated: 2015/01/16 00:35:39 by kperreau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,8 +56,8 @@ static int	ft_write_g(t_options *opt, char *s, char *s2)
 	len = ft_strlen(s);
 	if (opt->precise != -1)
 		opt->precise = ft_leetnlen(s, opt->precise);
-	printf("test: %d\n", opt->precise);
 	len = (opt->precise != -1 && len > opt->precise) ? opt->precise : len;
+	printf("test: %d\n", len);
 	if (opt->len > len && !(opt->flags & 1))
 		ft_putspace(opt, len, 0);
 	write(1, s, len);
