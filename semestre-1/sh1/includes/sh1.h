892f0d6327c66d31f9e0371ac9ff5e40eff3cc4e
Kevin Perreau <Keveun@gmail.com>
2015-01-19 16:47:44 +0100

    auto update

---
+++ semestre-1/sh1/includes/sh1.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sh1.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kperreau <kperreau@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/01/19 16:40:01 by kperreau          #+#    #+#             */
+/*   Updated: 2015/01/19 16:46:04 by kperreau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SH1_H
+# define SH1_H
+# include "libft.h"
+# include <stdio.h>
+# include <dirent.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include <sys/wait.h>
+# include <signal.h>
+
+
+int		ft_sh1(void);
+#endif
